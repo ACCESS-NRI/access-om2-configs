cd8b069ed031614af45613e3b232ca9af792dc4b
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2019-07-08 17:13:19 +1000

    improve sync_*

--- sync_restarts_to_gdata.sh
+++ sync_restarts_to_gdata.sh
@@ -4,8 +4,8 @@
 #PBS -l wd
 #PBS -l walltime=2:00:00,mem=2GB
 #PBS -P v45
-#PBS -N sync_restarts_to_gdata
+#PBS -N restarts_to_gdata
 
 source sync_output_to_gdata.sh # to define GDATADIR and cd archive
 
-rsync --ignore-existing -av --safe-links --no-g restart??[05]* ${GDATADIR}
+rsync -av --safe-links --no-g restart??[05]* ${GDATADIR}
