f9c89bfe1bed3eaabe54b33cf68bea500bd67fb5
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2019-07-09 11:15:39 +1000

    deleted env-wrapper.sh

--- env-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-log=$(pwd)/archive/env-wrapper_logs/${PBS_JOBID}
-mkdir -p ${log}
-world_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_RANK})
-local_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_LOCAL_RANK})
-envdump=environ-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
-env > ${log}/${envdump}
-exec "$@"
